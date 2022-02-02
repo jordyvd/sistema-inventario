6a7707b28ecff6b2568749b502ca5e6a25742f26
jordyvd <54369512+jordyvd@users.noreply.github.com>
2022-02-01 22:29:13 -0500

    commandScheduleWork

---
+++ app/Console/Commands/ScheduleWork.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+class ScheduleWork extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'schedule:work';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'crone en laravel';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Schedule worker started successfully.');
+        while (true) {
+            if ( date("Y-m-d H:i:s") == date("Y-m-d")." 22:40:00") {
+                $this->call('test:command');
+            }
+            sleep(1);
+        }
+    }
+}
