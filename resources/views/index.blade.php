16c1ac1d731ea7d52c214a1ddac00822a5659f1e
jordyvd <54369512+jordyvd@users.noreply.github.com>
2022-02-20 23:37:15 -0500

    changeVersion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,7 +50,7 @@
        </div>
     @endif
     <!-- importante -->
-       <script src="{{ asset('js/app.js?version=2022-02-05')}}"></script>
+       <script src="{{ asset('js/app.js?version=2022-02-20')}}"></script>
        <!-- <script src="{{ asset('js/app.js') }}"></script> -->
        <script src="{{asset('js/jquery.js')}}"></script>
        <script src="{{asset('js/main.js')}}"></script>
