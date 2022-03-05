40d4c353493fa77a054e98754c4c9968ee8f4ce9
jordyvd <54369512+jordyvd@users.noreply.github.com>
2022-03-05 00:18:34 -0500

    sp-mv

---
+++ database/migrations/2022_02_26_122560_sp_get_movimientos.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SpGetMovimientos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $procedure = "
+        CREATE PROCEDURE get_movimientos(in desde_p date, in hasta_p date, in tipo_p text, in sucursal_p text, in npage int, in perpage int)
+        begin
+	
+        set @count = (select count(*) from movimientos m
+        where m.sucursal = sucursal_p and m.tipo = tipo_p and m.fecha between desde_p and hasta_p);
+    
+        set npage = perpage*(npage-1);
+        
+            select m.descuento descuento, p.nompro , p.marca, m.precio , m.cantidad , m.fecha, m.condicion , m.detalle,m.sucursal,m.nro_documento, @count count
+            from movimientos m 
+            join products p on m.barra_mov = p.barra
+            where m.sucursal = sucursal_p and m.tipo = tipo_p and m.fecha between desde_p and hasta_p 
+            order by p.nompro asc limit perpage offset npage; 
+        END";
+        DB::unprepared('DROP PROCEDURE IF EXISTS get_movimientos');
+        DB::unprepared($procedure);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
