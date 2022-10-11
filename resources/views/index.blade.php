af32520c42dd9bd1dd35fbba410e2aebbc3aa8c2
Jack Sari <jack.sanchez@alarcongroup.us>
2022-10-11 02:04:16 -0500

    ....

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,7 +51,7 @@
        </div>
     @endif
     <!-- importante -->
-       <script src="{{ asset('js/app.js?version=2022-09-11 20:35')}}"></script>
+       <script src="{{ asset('js/app.js?version=2022-10-11 20:35')}}"></script>
        <!-- <script src="{{ asset('js/app.js') }}"></script> -->
        <script src="{{asset('js/jquery.js')}}"></script>
        <script src="{{asset('js/main.js')}}"></script>
