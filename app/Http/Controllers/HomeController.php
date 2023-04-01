460f1a670ec40f3a6e952e92db2d3c3cb866fa46
Jack Sari <jack.sanchez@alarcongroup.us>
2023-04-01 18:24:37 -0500

    ..

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -110,11 +110,13 @@ class HomeController extends Controller
         $salida = 0;
         foreach($data as $value){
             if($value->condicion == "salida"){
-                $salida += (($value->monto == null || $value->monto == "") ? 0 : $value->monto);
+                //$salida += (($value->monto == null || $value->monto == "") ? 0 : $value->monto);
+                $salida[] = $value->monto;
             }else{
                 if($value->condicion_cp !== 2)
                 {
-                    $ingreso += (($value->monto == null || $value->monto == "") ? 0 : $value->monto);
+                    $ingreso[] = $value->monto;
+                    //$ingreso += (($value->monto == null || $value->monto == "") ? 0 : $value->monto);
                 }
             }
         }
