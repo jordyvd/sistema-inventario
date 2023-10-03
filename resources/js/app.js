99377d363b58757473ba8a792bd61f2f93d57469
jordyvd <jordyverastegui64@gmail.com>
2023-10-03 17:11:03 -0500

    d

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,68 +1,88 @@
-require('./bootstrap');
-require('./pusher');
-window.Vue = require('vue');
+require("./bootstrap");
+// require('./pusher');
+window.Vue = require("vue");
 import VueHtmlToPaper from "vue-html-to-paper";
-import pdf from 'vue-pdf';
-import moment from 'moment';
-import Popover from 'vue-js-popover'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import pdf from "vue-pdf";
+import moment from "moment";
+import Popover from "vue-js-popover";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import 'bootstrap-vue/dist/bootstrap-vue.css'
- 
-Vue.use(Popover, { tooltip: true })
-Vue.use(BootstrapVue)
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(Popover, { tooltip: true });
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-Vue.component('login_user', require('./components/LoginComponent.vue').default);
-Vue.component('menu_sistema', require('./components/MenuComponent.vue').default);
-Vue.component('bajos-stock', require('./components/BajosStock.vue').default);
-Vue.component('grafico-ventas', require('./components/GraficoVentas.vue').default);
-Vue.component('grafico-ganancia', require('./components/GraficoGanancia.vue').default);
-Vue.component('grafico-clientes', require('./components/GraficoClientes.vue').default);
-Vue.component('grafico-vendedor', require('./components/GraficoVendedores.vue').default);
-Vue.component('grafico-proveedor', require('./components/GraficoProveedor.vue').default);
-Vue.component('grafico-Sinternas', require('./components/GraficoSinternas.vue').default);
-Vue.component('notpermiso', require('./components/NotpermisoComponent.vue').default);
-Vue.component('spinner', require('./components/Spinner.vue').default);
-// importar validacion de login 
-import auth from './auth';
+Vue.use(IconsPlugin);
+Vue.component("login_user", require("./components/LoginComponent.vue").default);
+Vue.component(
+    "menu_sistema",
+    require("./components/MenuComponent.vue").default
+);
+Vue.component("bajos-stock", require("./components/BajosStock.vue").default);
+Vue.component(
+    "grafico-ventas",
+    require("./components/GraficoVentas.vue").default
+);
+Vue.component(
+    "grafico-ganancia",
+    require("./components/GraficoGanancia.vue").default
+);
+Vue.component(
+    "grafico-clientes",
+    require("./components/GraficoClientes.vue").default
+);
+Vue.component(
+    "grafico-vendedor",
+    require("./components/GraficoVendedores.vue").default
+);
+Vue.component(
+    "grafico-proveedor",
+    require("./components/GraficoProveedor.vue").default
+);
+Vue.component(
+    "grafico-Sinternas",
+    require("./components/GraficoSinternas.vue").default
+);
+Vue.component(
+    "notpermiso",
+    require("./components/NotpermisoComponent.vue").default
+);
+Vue.component("spinner", require("./components/Spinner.vue").default);
+// importar validacion de login
+import auth from "./auth";
 Vue.mixin(auth);
 // function global
-import global_f from './function_global';
+import global_f from "./function_global";
 Vue.mixin(global_f);
 //PAGINACION
-import pagination from './pagination';
+import pagination from "./pagination";
 Vue.mixin(pagination);
 //sweetalert
-import swal from 'sweetalert';
+import swal from "sweetalert";
 // TOAST
-import VueToast from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-default.css';
+import VueToast from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-default.css";
 Vue.use(VueToast);
 // FIN TOAST
 // routers
-import router from './routes';
+import router from "./routes";
 const app = new Vue({
-    el: '#app',
-    router
+    el: "#app",
+    router,
 });
 // IMPRIMIR
 // ...
 const options = {
-    name: '_blank',
-    specs: [
-      'fullscreen=yes',
-      'titlebar=yes',
-      'scrollbars=yes'
-    ],
+    name: "_blank",
+    specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
     styles: [
-      'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-      'https://unpkg.com/kidlat-css/css/kidlat.css'
-    ]
-  }
-  
-  Vue.use(VueHtmlToPaper, options);
-  
-  // or, using the defaults with no stylesheet
-  Vue.use(VueHtmlToPaper);
-  // ...
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+        "https://unpkg.com/kidlat-css/css/kidlat.css",
+    ],
+};
+
+Vue.use(VueHtmlToPaper, options);
+
+// or, using the defaults with no stylesheet
+Vue.use(VueHtmlToPaper);
+// ...
